2841750eca7386c21a51a7af04ac11487191e4f4
Casey Wilcox <caseywilcox@outlook.com>
2016-06-07 17:45:39 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,1 @@
+What this webapp does is loads a web browser and shows a list of movies and trailers.
---
+++ media.py
@@ -0,0 +1,13 @@
+class Movie():
+    """This class provides a way to store movie related information."""  #  Used for __docs__ function
+
+# Initializing our attributes for class Movie to be used in entertainment_center.py
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube,
+                 movie_rating, movie_duration, movie_release):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.rating = movie_rating
+        self.duration = movie_duration
+        self.release = movie_release
